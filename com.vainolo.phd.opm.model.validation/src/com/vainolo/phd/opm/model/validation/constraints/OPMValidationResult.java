d289178c41847422bc1170fd0bc4a90acbca9bd7
Eyal <eyal.heinemann@gmail.com>
2012-09-18 19:13:21 +0300

    some lie for validation

---
+++ com.vainolo.phd.opm.model.validation/src/com/vainolo/phd/opm/model/validation/constraints/OPMValidationResult.java
@@ -0,0 +1,20 @@
+package com.vainolo.phd.opm.model.validation.constraints;
+
+enum OPMValidationResult{
+	/**
+	 * When the constraint cannot validate the Expression
+	 */
+	NoResult,
+	
+	/**
+	 * The Given expression is valid according to the constraint
+	 */
+	Valid,
+	
+	/**
+	 * The given expression is not valid according to the constraint
+	 */
+	InValid
+	
+	
+}
