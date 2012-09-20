b70e18b058bb2ef37c1e8cbc4ba078e77c320130
Eyal <eyal.heinemann@gmail.com>
2012-09-20 22:35:29 +0300

    merging arieh's work

---
+++ com.vainolo.phd.opm.validation/src/com/vainolo/phd/opm/validation/Specification/OrSpecification.java
@@ -0,0 +1,16 @@
+package com.vainolo.phd.opm.validation.Specification;
+
+public final class OrSpecification<T> extends CompositeSpecification<T> {
+
+private ISpecification<T> left,right;
+	
+	public OrSpecification(ISpecification<T> left, ISpecification<T> right){
+		this.left = left;
+		this.right = right;
+	}
+	
+	@Override
+	public boolean IsSatisfiedBy(T candidate) {
+		return (left.IsSatisfiedBy(candidate) || right.IsSatisfiedBy(candidate));
+	}
+}
---
+++ com.vainolo.phd.opm.validation/src/com/vainolo/phd/opm/validation/constraints/SomeLiePolicy.java
@@ -0,0 +1,26 @@
+package com.vainolo.phd.opm.validation.constraints;
+
+import com.vainolo.phd.opm.model.OPMLink;
+import com.vainolo.phd.opm.model.OPMNode;
+
+public class SomeLiePolicy {
+
+	
+	public boolean put(Class<?> linkType, Object source, Object target, boolean isValid){
+		if (!OPMLink.class.isAssignableFrom(linkType)) return false;
+		
+		
+		
+		return true;
+	}
+	
+	public boolean InquireIsAllowed(OPMLink link){
+		Class<?> linkType = link.getClass();
+		OPMNode source = link.getSource();
+		OPMNode target = link.getTarget();
+		
+		
+		return false;
+	}
+	
+}
